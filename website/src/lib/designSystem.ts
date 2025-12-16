f5bbebe46e1c213e3b107aeedb73b1891e4c4194
aloylee2 <161858776+aloylee2@users.noreply.github.com>
2025-12-16 21:14:17 +0800

    Initialize React + TypeScript Vite project
    Set up a new web application using React, TypeScript, and Vite. Added initial project structure, configuration files, ESLint setup, Tailwind CSS, and example pages and layouts.

---
+++ website/src/lib/designSystem.ts
@@ -0,0 +1,33 @@
+export const colors = {
+  primary: "#FF9B9B",
+  secondary: "#FFCCCC",
+  background: "#FFFFFF",
+  text: "#732727",
+  tabIcon: "#A2A2A2",
+  inputFieldBackground: "#EEEEEE",
+  success: "#34A853",
+  warning: "#FBBC05",
+  fail: "#FF0000",
+  white: "#FFFFFF",
+  black: "#000000",
+  lightGray: "#D3D3D3",
+  orange: "#f3a20a",
+};
+
+export const sizes = {
+  xs: 4,
+  s: 8,
+  m: 12,
+  l: 16,
+  xl: 20,
+  xxl: 24,
+};
+
+export const font = {
+  xs: 12,
+  s: 14,
+  m: 16,
+  l: 18,
+  xl: 20,
+  xxl: 24,
+};
