57e764742d6ef0bdcc64b2b5fe04cb7c80cb7296
YangHan20 <yh1318086601@gmail.com>
2025-12-18 19:12:13 +0800

    fix the receipe import

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ from app.features.getstream.stream_router import stream_router
 from app.features.journal.journal_router import journal_router
 from app.features.miscellaneous.misc_routes import misc_router
 from app.features.risk.risk_router import router as risk_router
+from app.features.recipes.recipe_router import recipe_router
 from app.schemas import UserCreate, UserRead, UserUpdate
 
 if not settings.APP_ENV:
